6286176bb2d8440eb6f2723c4dbb6ab87829d88e
pubkey <8926560+pubkey@users.noreply.github.com>
2024-01-30 17:33:30 +0100

    FIX release

--- scripts/version.js
+++ scripts/version.js
@@ -1,5 +1,13 @@
+const fs = require('node:fs');
+
 /**
- * Just prints the version
+ * Prints the version
+ * and set it to the package.json version
  */
 const packageJSON = require('../package.json');
+
+
+packageJSON.version = packageJSON.devDependencies.rxdb;
+fs.writeFileSync('./package.json', JSON.stringify(packageJSON, null, 2), 'utf-8');
+
 console.log(packageJSON.devDependencies.rxdb);
